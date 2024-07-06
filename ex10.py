0f9c7c9331d8f13a145c2cbb67cff931722de585
Brued <Bruedbes@gmail.com>
2024-07-06 09:39:57 -0300

    add ex10

--- ex10.py
+++ ex10.py
@@ -3,6 +3,6 @@
 # considere Uss 1,00 = R$ 3.27
 
 real = float(input("Insira a quantidade de dinheiro R$: "))
-dolar = real * 3.27 
-print('_' * 20)
-print(f'Tantos {real} equivalem a {dolar} dolares. ')
+dolar = real / 3.27 
+print('_' * 40)
+print(f'Tantos {real:.2f} equivalem a {dolar:.2f} dolares. ')
