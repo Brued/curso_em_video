7f87b9eb408ef646265178679a114c3d7cfab8df
Brued <Bruedbes@gmail.com>
2024-07-05 22:36:29 -0300

    add ex7

---
+++ ex7.py
@@ -0,0 +1,8 @@
+# DEsenvolva um programa que leia as duas notas de um aluno e calcule e mostre a sua média.
+
+n1 = int(input('Digite a primeira nota: '))
+n2 = int(input('Digite a segunda nota: '))
+
+media = (n1 + n2)/2
+print('_.' * 13)
+print(f'A média do aluno foi {media}')
