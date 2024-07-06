065af1b55e1fedf0b9aec70c278fc3d853990e51
Brued <Bruedbes@gmail.com>
2024-07-05 22:52:11 -0300

    add ex9

---
+++ ex9.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um num int qlqr e mostre na tela sua tabuada.
+
+num = int(input('Insira um numero: '))
+print('-_-' * 13)
+print('A tabuada de {num} é: ')
+print(f' {num} x 1 = {num*1} \n {num} x 2 = {num*2} \n {num} x 3 = {num*3} \n {num} x 4 = {num*4} \n {num} x 5 = {num*5} \n {num} x 6 = {num* 6} \n {num} x 7 = {num*7} \n {num} x 8 = {num*8} \n {num} x 9 = {num*9} \n {num} x 10 = {num*10}')
