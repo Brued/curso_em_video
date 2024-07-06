76234191d05a27f6fafd2b8674a05e97e9b3a2aa
Brued <Bruedbes@gmail.com>
2024-07-05 22:19:49 -0300

    add ex005

---
+++ ex005.py
@@ -0,0 +1,9 @@
+# faça um programa que leia um numero inteiro e mostre na tela o seu s
+#sucessor e eu antecessor.
+
+num = int(input('Digite um numéro: '))
+suc = num + 1
+ante = num -1
+print('_.' * 10)
+
+print(f'Seu sucessor é {suc} e seu antecessor é {ante}.')
