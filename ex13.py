36e1a6738d5a4b5aebad3889482730a3f4ae5d2a
Brued <Bruedbes@gmail.com>
2024-07-06 09:15:34 -0300

    add ex13

---
+++ ex13.py
@@ -0,0 +1,7 @@
+# faça um algoritmo que leia o salario de um funcionario
+# e mostre seu novo salario com 15% de aumento
+
+salario = float(input('Digite seu salário R$: '))
+print('*' *50)
+print(f"Seu salário R$ {salario} com aumento é de R$ {salario + salario*(15/100)}: ")
+print('*' *50)
