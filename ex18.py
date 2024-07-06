5782905a2b57e2821499f7fecfd53215bf6ea65d
Brued <Bruedbes@gmail.com>
2024-07-06 10:59:32 -0300

    add ex18_2

--- ex18.py
+++ ex18.py
@@ -2,4 +2,7 @@
 #mostre na tela o valor do seno, cosse e tangente desse angulo
 from math import sin, cos, tan
 angulo = float(input('Qual valor do algulo? '))
-print(f"Para o valor desse angulo, temos: \n O seno: {sin(angulo):.2f} \n O cosseno: {cos(angulo):.2f} \n A tangente:  {tan(angulo):.2f} ")
+print(f"Para o valor desse ângulo, temos:\n"
+      f"O seno: {sin(angulo):.2f}\n"
+      f"O cosseno: {cos(angulo):.2f}\n"
+      f"A tangente: {tan(angulo):.2f}")
