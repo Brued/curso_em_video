b243a27874974c003ebb01632198d892060b50cc
Brued <Bruedbes@gmail.com>
2024-07-05 22:31:58 -0300

    add ex6

---
+++ ex6.py
@@ -0,0 +1,8 @@
+# crie um algoritmo que leia um numero e mostre seu doobro, triplo e raiz quadrada.
+
+num = int(input('Digite um numero: '))
+dobro = num *2 
+triplo = num * 3
+raiz = num * (1/2)
+print('_.' * 10)
+print(f"O dobro de {num} é {dobro} ,\n O triplo de {num} é {triplo} e \n A raiz quadrada de {num} é {raiz}.")
