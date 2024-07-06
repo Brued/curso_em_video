42d62c6881a80214cb2f0ad0fb258d14282a7463
Brued <Bruedbes@gmail.com>
2024-07-06 10:39:30 -0300

    add ex16

---
+++ ex16.py
@@ -0,0 +1,4 @@
+# crie um programa que leia um numero Rel qlqr e mostre na tela sua porção inteira.
+from math import trunc
+num = float(input('Digite um numero Real: '))
+print(f'O número {num } tem parte inteira: {trunc(num)} ')
