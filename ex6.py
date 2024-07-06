201551050f8480d26ef118e8cd978d34276a2f1d
Brued <Bruedbes@gmail.com>
2024-07-06 09:23:19 -0300

    add ex6_2

--- ex6.py
+++ ex6.py
@@ -3,6 +3,6 @@
 num = int(input('Digite um numero: '))
 dobro = num *2 
 triplo = num * 3
-raiz = num * (1/2)
+raiz = num ** (1/2)
 print('_.' * 10)
 print(f"O dobro de {num} é {dobro} ,\n O triplo de {num} é {triplo} e \n A raiz quadrada de {num} é {raiz}.")
