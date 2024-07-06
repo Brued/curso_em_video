093bd66257bc91898d58bfabc02af185e3c9cb3a
Brued <Bruedbes@gmail.com>
2024-07-06 14:19:51 -0300

    add ex20

---
+++ ex20.py
@@ -0,0 +1,21 @@
+# o mesmo prof do ex19  quer sortear a ordem de apresentacao de trabalhos dos alunos.
+#faça um programa que leia o nome dos quatro aluno e mostre a ordem sorteada.
+from random import shuffle
+aluno1 = input('Digite o nome do primeiro aluno: ')
+aluno2 = input('Digite o nome do segundo aluno: ')
+aluno3 = input('Digite o nome do terceiro aluno: ')
+aluno4 = input('Digite o nome do quarto aluno: ' )
+
+list = [aluno1, aluno2, aluno3, aluno4]
+shuffle(list)
+print(f" Pelo sorteio quem apresentará\n " 
+      f"Primeiro: {list[0]}\n"
+      f"Segundo: {list[1]}\n"
+      f"Terceiro: {list[2]}\n"
+      f"Quarto: {list[3]}")
+
+
+# mylist = ["apple", "banana", "cherry"]
+# shuffle(mylist)
+
+# print(mylist)
