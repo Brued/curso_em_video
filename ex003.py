71a99678ccae81c0be02309055a322b7dcd911c5
Brued <Bruedbes@gmail.com>
2024-07-05 21:02:30 -0300

    add ex003

---
+++ ex003.py
@@ -0,0 +1,8 @@
+# crie um progragrama que leia dois numeros e mostre a soma entre eles
+
+x1= input('Digite um numero: ')
+x2= input('Digite um numero: ')
+
+print('--.--' * 10)
+soma = x1 + x2 
+print(f"A Soma entre {x1} e {x2} é: {soma}.")
