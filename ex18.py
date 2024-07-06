8d0cff0105bf49fa9ce2e5abe8099af5ef91c69e
Brued <Bruedbes@gmail.com>
2024-07-06 11:20:53 -0300

    add ex18_2

--- ex18.py
+++ ex18.py
@@ -1,8 +1,8 @@
 #Faça um programa que leia um angulo qlqr 
 #mostre na tela o valor do seno, cosse e tangente desse angulo
-from math import sin, cos, tan
+from math import sin, cos, tan, radians
 angulo = float(input('Qual valor do algulo? '))
 print(f"Para o valor desse ângulo, temos:\n"
-      f"O seno: {sin(angulo):.2f}\n"
-      f"O cosseno: {cos(angulo):.2f}\n"
-      f"A tangente: {tan(angulo):.2f}")
+      f"O seno: {sin(radians(angulo)):.2f}\n"
+      f"O cosseno: {cos(radians(angulo)):.2f}\n"
+      f"A tangente: {tan(radians(angulo)):.2f}")
