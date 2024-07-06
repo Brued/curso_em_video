ba2f0ae655f245ef3d8394ebfc48c72a74ae633e
Brued <Bruedbes@gmail.com>
2024-07-06 09:28:26 -0300

    add ex8_2

---
+++ ex8.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um valor em m e o exiba covertido em cm e mm.
+
+valor = int(input('Insira a medida em m: '))
+cm= valor*10
+mm= valor*100
+print('_;' *13)
+print(f'O valor de {valor} em cm é {cm} e em mm é {mm}')
