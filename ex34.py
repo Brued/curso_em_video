f90440a4b5c674abb753d39df26f2a67372d395b
Brued <Bruedbes@gmail.com>
2024-07-17 13:32:48 -0300

    add ex34

---
+++ ex34.py
@@ -0,0 +1,12 @@
+# Escreva um prog que pergunte o salario de um funcionario e calcule seu valor de aumento
+# para salarios superiores a 1.250 calcule 10% de aumento
+# para inferios ou iguais, aumento de 15%
+
+salario = float( input("Digite seu salário R$: "))
+
+if salario > 1250:
+    aumento = salario/10
+    print(f"O seu aumento é de: {aumento:.2f}")
+if salario <= 1250:
+    aumento = salario*15/10
+    print(f"O seu aumento é de: { aumento:.2f}")
