23c91632d7634367f29766790dc3c854abb17711
Brued <Bruedbes@gmail.com>
2024-07-17 14:17:59 -0300

    add ex33b

--- ex33.py
+++ ex33.py
@@ -4,21 +4,21 @@ x1 = int(input("Insira o primeiro numero: "))
 x2 = int(input("Insira o segundo numero: "))
 x3 = int(input("Insira o terceiro numero: "))
 
-x = x1
-if x2 > x:
-    x = x2
-if x3 > x:
-    x = x3
+menor = x1
+if x2 < x1 and x2 < x3:
+    menor = x2
+if x3 < x1 and x3 < x2:
+    menor = x3
 
 print("*-"*10)
-print(f"Maior é: {x}")
+print(f"Menor é: {menor}")
 
 print("*-"*10)
 
-x = x1
-if x2 > x:
-    x = x2
-if x3 > x :
-    x = x3
-print(f"Menor é: {x} ")
+maior = x1
+if x2 > x1 and x2 > x3:
+    maior = x2
+if x3 > x1 and x3 > x2 :
+    maior = x3
+print(f"Maior é: {maior} ")
 print("*-"*10)     
