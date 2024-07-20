dd76580a291ec5e85033f5f24907b0d4148e03d7
Brued <Bruedbes@gmail.com>
2024-07-20 13:55:53 -0300

    ex51v2

--- ex51.py
+++ ex51.py
@@ -7,7 +7,17 @@ novo_termo = primeiro_termo
 for i in range(0, 11):
     termos.append(novo_termo)
     novo_termo += razao
-    print(novo_termo)
-    
+    print(novo_termo, end=' ')
+
+print(' ')
+
+print('>.<'*30)
 print(f"os 10 primeiros termos são: {termos} ")
 # print('-'*15)
+
+####################################
+
+decimo_termo = primeiro + (10-1)*razao
+
+for c in range(primeiro_termo, decimo_termo, razao):
+    print(f"{c}", end = ' → ')
