1ce9dd35671dee33152c877925af36d7b88dc901
Brued <Bruedbes@gmail.com>
2024-07-20 13:30:10 -0300

    ex47v2

--- ex47.py
+++ ex47.py
@@ -1,4 +1,6 @@
 # crie um prog que mostre na tela todos os numeros pares que estao no intervalo (1,50)
+
+# modo 1 >>>MELHOR PQ TENHO MENOS ITERAÇÃO (MEMORIA)
 print('*-'*15)
 for i in range(1,50,2):
     print(i+1)
@@ -6,8 +8,8 @@ print('São os números pares.')
 
 # modo 2
 
-print(f'{"x":=^20}')
-for i in range(1,51):
-    if i%2==0:
-        print(i)
-print('São os números pares.')
+# print(f'{"x"*70}')
+# for i in range(1,51):
+#     if i%2==0:
+#         print(i, end= ' ')
+# print('São os números pares.')
--- ex53.py
+++ ex53.py
@@ -1,1 +1,9 @@
 # crie um prog que leia uma frase qlqr e dig se ela é um palindromo, desconsiderando os espços
+
+
+frase = str(input('Digite uma frase: ')).upper().strip()
+print(frase)
+
+fr = frase.count('A')
+
+print(fr)
