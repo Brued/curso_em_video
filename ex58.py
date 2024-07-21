599c278bbb3c866fd7fbcfb378b5018085f52846
Brued <Bruedbes@gmail.com>
2024-07-21 15:58:53 -0300

    ex58

--- ex58.py
+++ ex58.py
@@ -16,3 +16,16 @@ print(f'Número aleatorio Computador : {numPC}')
 print(f"Para acertar o você deu {c} palpites.")
 print('_.'*18)
 
+acertou = False
+palpites = 0
+while not acertou: 
+    jogador int(input('Qual o seu palpite?'))
+    palpites += 1
+    if jogador  == computador:
+        acertou = True
+    else: 
+        if jogador < computador:
+        print('Mais >>')
+    elif jogador > computador:
+        print ('Menos << ')
+print(f'Acertou com { palpites} tentativas ')
--- ex59.py
+++ ex59.py
@@ -1,4 +1,4 @@
-# crie um prog que leia dois valores e mostre um meno na tela
+# crie um prog que leia dois valores e mostre um menu na tela
 
 # 1- somar
 # 2 multiplicar
@@ -7,3 +7,32 @@
 # 5 sair do programa
 
 # seu prog devera realizar a operacao solicitada em casa caso.
+
+n1 = int (input('Digite um valor: '))
+n2 = int (input('Digite o outro valor: '))
+
+print('=.'*15)
+print("""[1] → SOMAR
+[2] → MULTIPLICAR
+[3] → NOVOS NÚMEROS
+[4] → SAIR DO PROGRAMA """)
+print('=.'*15)
+
+escolha = int(input('Qual a opção desejada? '))
+print(' ')
+
+if escolha == 1:
+    n = n1 + n2
+    print(f" → A soma de {n1} e {n2} é igual a {n}.")
+
+if escolha == 2:
+    nn = n1 * n2
+
+    print(f" → A multiplicação de {n1} e {n2} é igual a {nn}")
+if escolha == 3:
+    while escolha == 3:
+        n1 = int (input('Digite um valor: '))
+        n2 = int (input('Digite o outro valor: '))
+        escolha = int(input('Qual a opção desejada? '))
+
+# if escolha == 4:
