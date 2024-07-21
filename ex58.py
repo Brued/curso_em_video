766e77e058c590d7153a925bef96c6be17a256f5
Brued <Bruedbes@gmail.com>
2024-07-21 15:24:09 -0300

    ex58

--- ex58.py
+++ ex58.py
@@ -1,2 +1,18 @@
 # melhore o jogo do DESAFIO 28 onde o pc vai pensar em um num 0-10. so que agor ao jgdor vai tentar adivinhar ate acertar
-# mostrando no final quantos palpites foram necessarios para vender.
+# mostrando no final quantos palpites foram necessarios para vencer.
+
+from random import randint
+numPC = randint(0,10)
+
+
+numuser = int(input('Digite um número de 0 a 10: '))
+c=0
+while numPC != numuser:
+    numuser = int(input('Digite um número de 0 a 10: '))
+    c += 1
+
+print('_.'*18)
+print(f'Número aleatorio Computador : {numPC}')
+print(f"Para acertar o você deu {c} palpites.")
+print('_.'*18)
+
