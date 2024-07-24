49b1c3b0ccfad14aa1d0a97fff7fb4cb22a5e9e9
Brued <Bruedbes@gmail.com>
2024-07-23 23:06:37 -0300

    ex64

--- ex64.py
+++ ex64.py
@@ -1,2 +1,18 @@
-# crie um prog que leia varios numeros inteiros pelo tc. o prigrama so vai para qndo o usuario digitar o valor 999. q
+# crie um prog que leia varios numeros inteiros pelo tc. o programa so vai para qndo o usuario digitar o valor 999. q
 #que é a condicao de parada. No final mostre quantos numeros foram digitados e qual foi a soma entre eles (desconsiderando o flag)
+
+n= 0
+cont = 0
+s = 0
+n = int(input('Digite um número inteiro \033[36m(p/ parar digite 999)\033[m:'))
+while n != 999: 
+        
+        cont += 1
+        s += n
+        n = int(input('Digite um número inteiro \033[36m(p/ parar digite 999)\033[m:'))
+else: n == 999
+print(f' Foram digitados {cont} números e a soma entre eles é {s}')
+
+
+################ guanabara
+       
