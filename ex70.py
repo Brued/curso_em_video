f822e3542233fa12e61f3bdf0cbef92aaa12e2ee
Brued <Bruedbes@gmail.com>
2024-07-26 21:47:42 -0300

    ex70

--- ex70.py
+++ ex70.py
@@ -1,4 +1,32 @@
-# crie um prog que leia o nome e o preco de varios produtos. o prog deverá perguntr se o usuario quer continuar e no finl moestra
+# crie um prog que leia o nome e o preco de varios produtos. o prog deverá perguntr se o usuario quer continuar e no final mostra
 # A qual é o total gasto na compra
 # B quantos produtos custam mais de 1000RS
 # C qual é o nome do produto mais barato
+
+
+s = i = 0
+barato = None
+nome_barato = ''
+while True: 
+    nome = str(input('Digite o nome do produto: '))
+    preco = float(input('Digite o preço do produto: '))
+    continuar = ' '
+    s += preco
+    if barato is None or preco < barato:
+        barato = preco
+        nome_barato = nome
+
+    while continuar not in 'SN':
+        continuar = str(input('\033[33m>>>Deseja continuar?[S/N]\033[m')).strip().upper()
+    if continuar == 'N':
+        break
+    
+    if preco > 1000:
+        i += 1
+
+    
+print('°°° Vamos analisar sua compra °°°')
+print(f""">>> Total gasto na compra foi de {s}
+>>> {i} produtos custam mais que R$ 1.000,00
+>>> o nome do produto mais barata é {nome_barato} com valor de {barato}
+""")
