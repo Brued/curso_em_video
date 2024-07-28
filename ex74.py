eb1ab90d701a4a1a6cd08abb4401c2acef53f3a4
Brued <Bruedbes@gmail.com>
2024-07-28 15:15:19 -0300

    ex74v2

--- ex74.py
+++ ex74.py
@@ -3,21 +3,27 @@
 
 from random import randint
 
-# t = ()
-# for i in range (0,5):
-#     num = randint(0,10)
-#     t.append(num)
-#     print(num)
-# print(t)
-
 from random import randint
+numeros = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+print (f'Os valores sorteados foram {numeros}', end='')
+print(f'\nO maior valor sorteado foi {max(numeros)} e o menor {min(numeros)}')
 
-# Gerar cinco números aleatórios e colocá-los em uma tupla
-num_tuple = tuple(randint(0, 10) for _ in range(5))
+################
+# # t = ()
+# # for i in range (0,5):
+# #     num = randint(0,10)
+# #     t.append(num)
+# #     print(num)
+# # print(t)
 
-# Mostrar a listagem dos números gerados
-print("Números gerados:", num_tuple)
+# from random import randint
 
-# Encontrar e mostrar o menor e o maior valor na tupla
-print("Menor valor:", min(num_tuple))
-print("Maior valor:", max(num_tuple))
+# # Gerar cinco números aleatórios e colocá-los em uma tupla
+# num_tuple = tuple(randint(0, 10) for _ in range(5))
+
+# # Mostrar a listagem dos números gerados
+# print("Números gerados:", num_tuple)
+
+# # Encontrar e mostrar o menor e o maior valor na tupla
+# print("Menor valor:", min(num_tuple))
+# print("Maior valor:", max(num_tuple))
--- ex77.py
+++ ex77.py
@@ -1,2 +1,15 @@
 # crie um prog que tenha um tupla com varias palavras (sem acentos). Depois disso, vc deve mostrar,
 # para cada palavra, quais sao as suas vogais
+
+
+
+    # Definir a tupla de palavras
+tupla = ('computador', 'league','casa', 'fisica', 'deus', 'jesus', 'pai' )
+
+for palavra in tupla:
+    vogais_encontradas = ()
+    for letra in palavra:
+        if letra in vogais:
+            vogais_encontradas.add(letra)
+
+print(f"Na palavra '{palavra}' as vogais encontradas são: {', '.join(vogais_encontradas) if vogais_encontradas else 'nenhuma'}")
