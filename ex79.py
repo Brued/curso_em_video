eb5e78694e6b5f34623bdb75a156977b87521191
Brued <Bruedbes@gmail.com>
2024-07-29 20:28:58 -0300

    ex79

--- ex79.py
+++ ex79.py
@@ -1,3 +1,20 @@
 # crie um prog que o usuario possa digitar varios valores numericos e cadastre-os em uma lista. 
 # Caso o numero ja exista la dentro, ele nao sera adicionado. No final, serao exibidos todos os valores únicos digitados,
 # em ordem crescente.
+
+lista = list()
+
+while True:
+    num = int(input('Digite um número:'))
+    if num not in lista:
+        lista.append(num)
+    opcao = str(input('Deseja continuar? [S/N] ')).upper().strip()
+    while opcao not in 'SN':
+        print('->> OPS. Digite uma opção válida.')
+        opcao = str(input('Deseja continuar? [S/N] ')).upper().strip()
+    if opcao == 'N':
+        break
+lista.sort()
+print('*'*50)
+print(f'Os números em ordem crescente será: {lista}')
+print('*'*50)
