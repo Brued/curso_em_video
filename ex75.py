1b86a95c674dba84d45ebbc504f2872da6055563
Brued <Bruedbes@gmail.com>
2024-07-28 22:33:58 -0300

    ex76

--- ex75.py
+++ ex75.py
@@ -41,21 +41,21 @@
 # print('-.'*12)
 
 ###################### guanabara
-num = ( int(input('Digite o primeiro numero: ')), 
-        int(input('Digite o segundo  numero: ')), 
-        int(input('Digite o terceiro numero: ')),  
-        int(input('Digite o quarto   numero: ')))
+# num = ( int(input('Digite o primeiro numero: ')), 
+#         int(input('Digite o segundo  numero: ')), 
+#         int(input('Digite o terceiro numero: ')),  
+#         int(input('Digite o quarto   numero: ')))
 
-print(f"Você digitou os valores {num}")
+# print(f"Você digitou os valores {num}")
 
-print(f'O valor 9 apareceu {num.count(9)} vezes')
+# print(f'O valor 9 apareceu {num.count(9)} vezes')
 
-if 3 in num:
-    print(f" o Valor 3 apareceu na {num.index(3) + 1}ª posição")
-else: 
-    print('O valore 3 não foi digitado em nenhuma posição')
+# if 3 in num:
+#     print(f" o Valor 3 apareceu na {num.index(3) + 1}ª posição")
+# else: 
+#     print('O valore 3 não foi digitado em nenhuma posição')
 
-print('Os valores pares digitados foram ', end='')
-for n in num:
-    if n % 2 ==0:
-        print(→ n , end='')
+# print('Os valores pares digitados foram ', end='')
+# for n in num:
+#     if n % 2 ==0:
+#         print(→ n , end='')
--- ex76.py
+++ ex76.py
@@ -1,2 +1,22 @@
-crie um programa que tenha uma tupla unica com nomes de produtos e seus respectivos precos na sequencia.
-No final, mostre uma listagem de precos organizando os dados em forma tabular.
+# crie um programa que tenha uma tupla unica com nomes de produtos e seus respectivos precos na sequencia.
+# No final, mostre uma listagem de precos organizando os dados em forma tabular.
+
+tp = ('Camiseta', 21.99,
+      'Óculos', 15.99,
+      'Calça', 79.99,
+      'Bermuda', 59.99,
+      'Tenis', 99.99,
+      'Camiseta', 49.99)
+
+print('*'*30)
+print('      Tabela de preços:')
+print('*'*30)
+
+
+for i in range (0, len(tp)):
+    if i % 2 == 0:
+        print(f"{tp[i]:.<30}", end='')
+    else:
+        print(f'R$ {tp[i]:5.2f}')
+
+
