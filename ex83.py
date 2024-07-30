246822501220e0955625343ddf137372ab52e003
Brued <Bruedbes@gmail.com>
2024-07-29 23:14:17 -0300

    ex83*

--- ex83.py
+++ ex83.py
@@ -2,3 +2,20 @@
 # passada esta com os parenteses abertos e fechado na ordem correta
 
 exp = str(input('Digite uma expressão: '))
+pilha = []
+for simb in exp:
+    if simb == '()':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressção está válida!')
+else:
+    print('Sua expressção está errada!')
+    
+    
+    
