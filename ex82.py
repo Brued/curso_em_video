42d1fc348d90a33a543464b5f2a47bec9ec27471
Brued <Bruedbes@gmail.com>
2024-07-29 22:57:59 -0300

    ex82

--- ex82.py
+++ ex82.py
@@ -1,3 +1,25 @@
 # crie um programa que vai ler  varios numeros e colocar em uma lsita.
 # Depois disso, crie duas listas extras que vao conter apenas os valores pares e os valores impares digitados respectivamente.
 # Ao final mostre o conteudo das 3 listas geradas
+
+
+lista = []
+lpar = []
+limpar =[]
+while True:
+    n = int(input('Insira um número: '))
+    lista.append(n)
+    sn = str(input('Deseja continuar?[S/N] ')).upper().strip()
+    if n % 2 == 0:
+        lpar.append(n)
+    else:
+        limpar.append(n)
+    if sn not in 'SN':
+        print('Tente novamente.')
+        sn = str(input('Deseja continuar?[S/N] '))
+    if sn == 'N':
+        break
+print('='*25)
+print(f'Lista é {lista}')
+print(f'Lista par → {lpar} ')
+print(f'Lista ímpar → {limpar}')
--- ex83.py
+++ ex83.py
@@ -1,2 +1,4 @@
 # Crie um programa onde o usuário digite uma expressao quaquer que use parenteses. Seu aplicativo deverá analisar se a expressao
 # passada esta com os parenteses abertos e fechado na ordem correta
+
+exp = str(input('Digite uma expressão: '))
