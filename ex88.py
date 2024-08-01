06114139ee77f83768cac4fcd587482c9991fc5b
Brued <Bruedbes@gmail.com>
2024-07-31 23:10:27 -0300

    ex88v2

--- ex88.py
+++ ex88.py
@@ -19,3 +19,25 @@ for c in range(quant_jogos):
 print()
 print(f'>> Essa é a lista de {quant_jogos} jogos {sorted(jogos)}')
 
+
+# #############
+# from random import randint
+# lista = []
+# jogos = []
+# quant =  int(input('Qts jogos voce quer que eu sorteie?'))
+# tot = 1
+# while tot <= quant:
+#     cont =0
+#     while True:
+#         num = randint(1, 60)
+#         if num not in lista:
+#             lista.append(num)
+#             cont +=1
+#         if cont >= 6:
+#             break
+#     list.sort()
+#     jogos.append(lista[:])
+#     lista.clear()
+#     tot +=1
+# for i, l in enumerate(jogos):
+#     print(f'Jogo {i+1}:{l}')
