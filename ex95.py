65247695e54a60df94a6dc3acb59d3f7acf8dc0a
Brued <Bruedbes@gmail.com>
2024-08-03 15:21:41 -0300

    ex95**

--- ex95.py
+++ ex95.py
@@ -1,51 +1,69 @@
-c_ps = sum_idade = 0
-dic = {}
-lista = []
+# aprimora o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualizacao de detalhes do aproveitamento
+# de cada jogador.
 
+
+jogador = dict()
+partidas = list()
+jogadores = list()
 while True:
-    nome = str(input('Nome: ')).strip()
-    sexo = str(input('Sexo [M/F]: ')).upper().strip()
-    while sexo not in 'MF':
-        sexo = str(input('Sexo inválido! Digite novamente [M/F]: ')).strip().upper()
+    jogador['nome'] = str(input('Nome do Jogador: ')).upper()
+    total_partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 
-    idade = int(input('Idade: '))
-    sum_idade += idade
+    for c in range(0, total_partidas):
+        partidas.append(int(input(f'    Quantos gols na partida {c+1}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    
+    jogadores.append(jogador.copy())
 
-    dic['Nome'] = nome
-    dic['Sexo'] = sexo
-    dic['Idade'] = idade
-    lista.append(dic.copy())
-    c_ps += 1 
-
-    choice = str(input('Deseja continuar [S/N]?_')).upper().strip()
-    while choice not in 'SN':
-        choice = str(input('Deseja continuar [S/N]?_')).upper().strip()
-
-    if choice == 'N':
+    while True:
+        continuar = str(input('Deseja continuar[S/N]? ')).upper()
+        if continuar in 'SN':
+            break
+        print('   -> Tente novamente.')
+    if continuar == 'N':
         break
 
-m_idd = sum_idade / c_ps
 
-print()
-print('*' * 30)
-print('----- Resultados da Análise -----')
-print()
-print(f'=> Total de pessoas cadastradas: {c_ps}')
-print(f'=> Média das idades: {m_idd:.2f}')
-print('...... Mulheres cadastradas ......')
-list_woman = []
-for pessoa in lista:
-    if pessoa['Sexo'] == 'F':
-        list_woman.append(pessoa['Nome'])
-print(f'=> {list_woman}')
 
-print()
-print('... Pessoas com idade acima da média ...')
-list_age = []
-for pessoa in lista:
-    if pessoa['Idade'] > m_idd:
-        list_age.append(pessoa)
-for pessoa in list_age:
-    print(f'=> Nome: {pessoa["Nome"]}, Sexo: {pessoa["Sexo"]}, Idade: {pessoa["Idade"]}')
 
-print('*' * 30)
+print('-='*50)
+print(jogadores)
+print('-='*50)
+
+print('------- Dados dos Jogadores -------')
+print('cod ', end = '')
+for i in jogador.keys():
+    print(f' {i:<14}', end = '')
+print()
+print('-'*40)
+for k, v in enumerate(jogadores):
+    print(f'{ k:>3}', end='')
+    for d in v.values():
+        print(f' {str(d):<15}', end = '')
+    print()
+print('-'*40)
+
+
+while True:
+    busca =  int(input('Mostrar dados de qual jogador?\n(999para parar)'))
+    if busca == 999:
+        break
+    if busca >= len(jogadores):
+        print(f'ERRO! não existe jogador com código {busca} ')
+    else:
+        print(f'Dados do jogador → {jogadores[busca]["nome"]}')
+        for i, g in enumerate(jogadores[busca]['gols']):
+            print(f'   No jogo {i+1} fez {g} gols;')
+    print('-'*40)
+print('Falouws')
+
+
+# for j in jogadores:
+#     for k, v in jogador.items():
+#         print(f'- O campo {k} tem o valor {v}')
+#     print('-='*30)
+#     print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+#     for i, v in enumerate (jogador['gols']):
+#         print(f'   => Na partida {i}, fez {v} goals.')
+#     print(f'Foi um total de {jogador["total"]} gols.')
