c2ee3f77bf141e0e09507e91adc66e18673535b6
Brued <Bruedbes@gmail.com>
2024-08-04 17:51:11 -0300

    ex102

--- ex102.py
+++ ex102.py
@@ -1,3 +1,17 @@
 # crie um programa que tenha uma função fatorial que receba dois paramentros: o primeiro que indique o numero a calcular e
 # o outro chamado show que será uma valor lógico(opcional) indicando se será mostrado ou nao na tela o processo de calculo 
 # do fatorial.
+
+def fatorial(n, show = False):
+    f=1
+    for i in range(n, 0, -1):
+        if show:
+            print(i, end ='')
+            if i > 1:
+                print('.', end = '')
+            else:
+                print(' = ', end = '')
+        f *= i
+    return f
+    
+print(fatorial(3, True))
