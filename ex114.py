369d0402fc3fd03fe4f348e7d1c42cd8d2f3c473
Brued <Bruedbes@gmail.com>
2024-08-09 16:22:03 -0300

    ex114

--- ex114.py
+++ ex114.py
@@ -1,1 +1,10 @@
 # 114: Crie um código em Python que teste se o site pudim está acessível pelo computador usado.
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('https://www.youtube.com/')
+except urllib.error.URLError:
+    print('O site do yt não está acessível no momento.')
+else:
+    print('Consegui acessar o site com sucesso.')
